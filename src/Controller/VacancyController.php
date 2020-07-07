f653f8bab78832197a1dc3bd5a7fd8a2a029c1d4
aghorianducalis <aghorianducalis@gmail.com>
2020-07-07 17:35:01 +0300

    removed unnecessary code

--- src/Controller/VacancyController.php
+++ src/Controller/VacancyController.php
@@ -4,13 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Site;
 use App\Entity\Vacancy;
-use App\Message\ParseVacancyMessage;
 use App\Service\VacancyProvider;
 use Doctrine\Persistence\ObjectManager;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VacancyController extends AbstractController
@@ -31,22 +28,8 @@ class VacancyController extends AbstractController
      * @Route("/vacancy", name="vacancy")
      * @return Response
      */
-    public function index(MessageBusInterface $bus, LoggerInterface $logger)
+    public function index()
     {
-        $logger->error(1244);
-        die();
-        $redis = new \Redis();
-        $redis->connect('127.0.0.1', 6379);
-
-        $message = new ParseVacancyMessage('/some_url');
-
-//        echo "<pre>";
-//        var_dump($redis->exists('messages'));
-//        var_dump($redis->sMembers('messages'));
-        $bus->dispatch($message);
-//        var_dump($redis->xLen('messages'));
-//        var_dump($bus->dispatch($message));
-        die();
         /** @var ObjectManager $entityManager */
         $entityManager = $this->getDoctrine()->getManager();
 
